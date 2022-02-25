de83954281389b7f020e9e596d02f5316b639257
Carsten Mußenbrock <carsten.mussenbrock@arvato-scs.com>
2022-02-25 12:09:00 +0100

    fix test

--- src/test/java/com/battlesnake/starter/SnakeTest.java
+++ src/test/java/com/battlesnake/starter/SnakeTest.java
@@ -33,7 +33,7 @@ public class SnakeTest {
     void indexTest() throws IOException {
 
         Map<String, String> response = handler.index();
-        assertEquals("#888888", response.get("color"));
+        assertEquals("#B00B69", response.get("color"));
         assertEquals("default", response.get("head"));
         assertEquals("default", response.get("tail"));
     }
@@ -52,7 +52,7 @@ public class SnakeTest {
                 "{\"game\":{\"id\":\"game-00fe20da-94ad-11ea-bb37\",\"ruleset\":{\"name\":\"standard\",\"version\":\"v.1.2.3\"},\"timeout\":500},\"turn\":14,\"board\":{\"height\":11,\"width\":11,\"food\":[{\"x\":5,\"y\":5},{\"x\":9,\"y\":0},{\"x\":2,\"y\":6}],\"hazards\":[{\"x\":3,\"y\":2}],\"snakes\":[{\"id\":\"snake-508e96ac-94ad-11ea-bb37\",\"name\":\"My Snake\",\"health\":54,\"body\":[{\"x\":0,\"y\":0},{\"x\":1,\"y\":0},{\"x\":2,\"y\":0}],\"latency\":\"111\",\"head\":{\"x\":0,\"y\":0},\"length\":3,\"shout\":\"why are we shouting??\",\"squad\":\"\"},{\"id\":\"snake-b67f4906-94ae-11ea-bb37\",\"name\":\"Another Snake\",\"health\":16,\"body\":[{\"x\":5,\"y\":4},{\"x\":5,\"y\":3},{\"x\":6,\"y\":3},{\"x\":6,\"y\":2}],\"latency\":\"222\",\"head\":{\"x\":5,\"y\":4},\"length\":4,\"shout\":\"I'm not really sure...\",\"squad\":\"\"}]},\"you\":{\"id\":\"snake-508e96ac-94ad-11ea-bb37\",\"name\":\"My Snake\",\"health\":54,\"body\":[{\"x\":0,\"y\":0},{\"x\":1,\"y\":0},{\"x\":2,\"y\":0}],\"latency\":\"111\",\"head\":{\"x\":0,\"y\":0},\"length\":3,\"shout\":\"why are we shouting??\",\"squad\":\"\"}}");
         Map<String, String> response = handler.move(moveRequest);
 
-        List<String> options = new ArrayList<String>();
+        List<String> options = new ArrayList<>();
         options.add("up");
         options.add("down");
         options.add("left");
@@ -79,8 +79,8 @@ public class SnakeTest {
 
         handler.avoidMyNeck(testHead, testBody, possibleMoves);
 
-        assertTrue(possibleMoves.size() == 4);
-        assertTrue(possibleMoves.equals(expectedResult));
+        assertEquals(4, possibleMoves.size());
+        assertEquals(possibleMoves, expectedResult);
     }
 
     @Test
@@ -94,8 +94,8 @@ public class SnakeTest {
 
         handler.avoidMyNeck(testHead, testBody, possibleMoves);
 
-        assertTrue(possibleMoves.size() == 3);
-        assertTrue(possibleMoves.equals(expectedResult));
+        assertEquals(3, possibleMoves.size());
+        assertEquals(possibleMoves, expectedResult);
     }
 
     @Test
@@ -109,8 +109,8 @@ public class SnakeTest {
 
         handler.avoidMyNeck(testHead, testBody, possibleMoves);
 
-        assertTrue(possibleMoves.size() == 3);
-        assertTrue(possibleMoves.equals(expectedResult));
+        assertEquals(3, possibleMoves.size());
+        assertEquals(possibleMoves, expectedResult);
     }
 
     @Test
@@ -124,8 +124,8 @@ public class SnakeTest {
 
         handler.avoidMyNeck(testHead, testBody, possibleMoves);
 
-        assertTrue(possibleMoves.size() == 3);
-        assertTrue(possibleMoves.equals(expectedResult));
+        assertEquals(3, possibleMoves.size());
+        assertEquals(possibleMoves, expectedResult);
     }
 
     @Test
@@ -139,7 +139,7 @@ public class SnakeTest {
 
         handler.avoidMyNeck(testHead, testBody, possibleMoves);
 
-        assertTrue(possibleMoves.size() == 3);
-        assertTrue(possibleMoves.equals(expectedResult));
+        assertEquals(3, possibleMoves.size());
+        assertEquals(possibleMoves, expectedResult);
     }
 }
