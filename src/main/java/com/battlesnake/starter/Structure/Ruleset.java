300335165451e7ad6b7977e415ddb3ff12113342
Carsten Mußenbrock <carsten.mussenbrock@arvato-scs.com>
2022-02-26 22:10:26 +0100

    remove settings

--- src/main/java/com/battlesnake/starter/Structure/Ruleset.java
+++ src/main/java/com/battlesnake/starter/Structure/Ruleset.java
@@ -3,5 +3,4 @@ package com.battlesnake.starter.Structure;
 public class Ruleset {
     public String name;
     public String version;
-    public Settings setting;
 }
--- src/main/java/com/battlesnake/starter/Structure/Settings.java
+++
@@ -1,9 +0,0 @@
-package com.battlesnake.starter.Structure;
-
-public class Settings {
-    public long foodSpawnChance;
-    public long minimumFood;
-    public long hazardDamagePerTurn;
-    public Royale royale;
-    public Squad squad;
-}
