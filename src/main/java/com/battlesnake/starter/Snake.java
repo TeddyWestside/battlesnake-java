0e67378e533bf28780b716614acdbb6715292798
Carsten Mußenbrock <carsten.mussenbrock@arvato-scs.com>
2022-02-27 13:41:23 +0100

    add logs

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -143,6 +143,8 @@ public class Snake {
         }
 
         private void avoidColisionWithSelf(Battlesnake you, Board board, ArrayList<String> possibleMoves) {
+            LOG.info("avoidColisionWithSelf you: {}", you.toString());
+            LOG.info("avoidColisionWithSelf board: {}", board.toString());
             for (int i = 0; i < board.snakes.length; i++) {
                 if (checkCoordFree(you.head.x + 1, you.head.y, board.snakes)) possibleMoves.remove("right");
                 if (checkCoordFree(you.head.x - 1, you.head.y, board.snakes)) possibleMoves.remove("left");
